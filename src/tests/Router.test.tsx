12fa632ce93f2232069e8f49dc52dac058d2afb7
shish-ko <andrey.shishko@gmail.com>
2023-03-18 00:14:27 +0300

    refactor: remove unused vars

--- src/tests/Router.test.tsx
+++ src/tests/Router.test.tsx
@@ -1,9 +1,8 @@
-import { render, screen, fireEvent, cleanup } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 import '@testing-library/jest-dom';
 import Router from '../router';
-import { useLocation } from 'react-router-dom';
 
 describe('Router', () => {
   it('renders main page', () => {
